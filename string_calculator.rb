8c5f3bbb5b3433adca6451828b4d35503a2bcbab
kanha55 <kanhu.dubey9@gmail.com>
2025-04-25 13:08:52 +0530

    Refactor add method into smaller sub-methods for improved readability

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,12 +1,29 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers = numbers.split(',')
-    
-    raise ArgumentError, "Input is invalid" if numbers.include?("\n")
-    
-    numbers = numbers.flat_map { |number| number.split("\n") }
 
+    numbers = split_by_comma(numbers)
+    validate_numbers(numbers)
+    numbers = split_by_newline(numbers)
+
+    sum_numbers(numbers)
+  end
+
+  private
+
+  def split_by_comma(numbers)
+    numbers.split(',')
+  end
+
+  def validate_numbers(numbers)
+    raise ArgumentError, "Input is invalid" if numbers.include?("\n")
+  end
+
+  def split_by_newline(numbers)
+    numbers.flat_map { |number| number.split("\n") }
+  end
+
+  def sum_numbers(numbers)
     numbers.map(&:to_i).sum
   end
 end
