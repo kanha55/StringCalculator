47530f5e0f83a5c4aaf93e67c8fb90671c078aa9
kanha55 <kanhu.dubey9@gmail.com>
2025-04-25 13:06:46 +0530

    Add test case for two number input in StringCalculator

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,13 @@
+
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+
+    if numbers.include?(',')
+      numbers = numbers.split(',')
+      return numbers[0].to_i + numbers[1].to_i
+    end  
+
     return numbers.to_i
   end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe StringCalculator do
     it "returns n for input string 'n' where n is any positive number" do
       expect(StringCalculator.new.add('98888888888')).to eq(98888888888)
     end 
+
+    it "returns 6 for input string '1,5'" do
+      expect(StringCalculator.new.add('1,5')).to eq(6)
+    end 
+
+    it "returns sum of two numbers for input string  of comma separated two numbers" do
+      expect(StringCalculator.new.add('9999,1')).to eq(10000)
+    end 
   end  
 end
