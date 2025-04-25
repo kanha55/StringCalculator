0fab96eed9e24118f222075d3967939f352021f3
kanha55 <kanhu.dubey9@gmail.com>
2025-04-25 13:07:04 +0530

    Refactor add method to handle two comma-separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,13 +1,15 @@
 
 class StringCalculator
   def add(numbers)
-    return 0 if numbers.empty?
 
-    if numbers.include?(',')
+    if numbers.empty?
+      return 0
+    elsif numbers.include?(',')
       numbers = numbers.split(',')
       return numbers[0].to_i + numbers[1].to_i
-    end  
-
-    return numbers.to_i
+    else
+      return numbers.to_i
+    end 
+     
   end
 end
