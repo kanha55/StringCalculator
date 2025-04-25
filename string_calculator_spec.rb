8a7d3d25d6690807b08975df28e412d7a3d200e4
kanha55 <kanhu.dubey9@gmail.com>
2025-04-25 13:05:11 +0530

    Add test for empty string input in StringCalculator

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,5 +1,9 @@
-RSpec.describe StringCalculator do
-  describe "#nothing" do
+require_relative 'string_calculator'
 
+RSpec.describe StringCalculator do
+  describe "#add" do
+    it 'returns 0 for empty string' do
+      expect(StringCalculator.new.add('')).to eq(0)
+    end      
   end  
 end
